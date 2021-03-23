d59bc49f44fe8bc332a5ff0ab8b5b26d50dc577e
tekuto1437 <denisjerosenko79@gmail.com>
2021-03-23 14:36:00 +0200

    Task1

--- 01-show-one-element/QUESTIONS.md
+++ 01-show-one-element/QUESTIONS.md
@@ -6,16 +6,16 @@
 
 > If you click the link to reveal more text and then refresh the page, does the text remain revealed, or is it hidden again? Why?
 
-Your reply here...
+Noklikšķinot uz saites, teksts tiek paslēpts. Atsvaidzinot lapu, tā tiek atjaunota stāvoklī, kurā klikšķis nav noticis, tāpēc teksts joprojām tiek paslēpts.
 
 ---
 
 > Remove `window.addEventListener("load", function(){` (and the closing `})`) from **global.js**. Does the link still reveal the text? What is the purpose of this code that you've removed?
 
-Your reply here...
+Būtībā jūs noņemat notikumu, kas tiek ielādēts neatkarīgi no tā. Šajā gadījumā window.onload ielādē visu JS, kad lapa tiek ielādēta. Bez tā tā vienkārši nav izsaucama funkcija.
 
 ---
 
 > Describe the the `addEventListener` method. (Remember that there is a specific, technical, methodical way to describe methods. Your reply should match that format.)
 
-Your reply here...
+You need to tell it what event it's listening for and what to do when the event occurs.
--- 01-show-one-element/global.js
+++ 01-show-one-element/global.js
@@ -1,17 +1,22 @@
-window.addEventListener("load", function(){
+window.onload = function() {
 
   // Here is some pseudo-code to help you get started:
 
   // 1. Get the DOM element which will be clicked.
+  var clickIt = document.getElementById("more_text_link");
 
   // 2. Add a listener for the 'click' event onto that element.
+  clickIt.addEventListener("click", function(){
 
-  // 3. The block for the listener should get the DOM
-  //    element containing the text to reveal.
+    // 3. The block for the listener should get the DOM
+    //    element containing the text to reveal.
+    var textToShow = document.getElementById("more_text_content");
 
-  // 4. Modify that DOM element's style to change it's 'display'
-  //    from a hidden value to a shown value.
+    // 4. Modify that DOM element's style to change it's 'display'
+    //    from a hidden value to a shown value.  
+    textToShow.style.display = "inline"
 
-  // 5. Also modify the DOM to hide the "More info..." link.
-  
-});
+    // 5. Also modify the DOM to hide the "More info..." link.
+    clickIt.style.display = "none"
+  });
+}; 
