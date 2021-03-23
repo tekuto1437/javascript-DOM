66130edc5e8ebc5591750b83d8e216f62d27766c
tekuto1437 <denisjerosenko79@gmail.com>
2021-03-23 15:49:53 +0200

    Task7

--- 07-automatic-tabbing-fields/QUESTIONS.md
+++ 07-automatic-tabbing-fields/QUESTIONS.md
@@ -6,4 +6,4 @@
 
 > What are some refactoring techniques you might apply to the provided solution?
 
-Your reply here...
+Tā kā metode kursora pārvietošanai no pirmās šūnas uz otro ir identiska metodei, ko izmanto, lai pārvietotu kursoru no otrās šūnas uz trešo, mainot tikai ID, es droši vien balsotu par ID ievietošanu masīvā un iziešanu to. Pašreizējais risinājums nav sauss.
--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,24 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+    var first = document.getElementById("first");
+    var second = document.getElementById("second");
+    var third = document.getElementById("third");
+  
+    first.addEventListener("keyup", function(){
+      input_char = first.value.length;
+      full_cell = parseInt(first.getAttribute("maxlength"));
+  
+      if (input_char === full_cell) {
+        second.focus();
+      }
+    });
+    second.addEventListener("keyup", function(){
+        input_char = second.value.length;
+        full_cell = parseInt(second.getAttribute("maxlength"));
+    
+        if (input_char === full_cell) {
+          third.focus();
+        }
+      });
+    
+    };
