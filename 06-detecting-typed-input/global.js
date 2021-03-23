cb0a81987ff1db4fb7f449c069849d5ed7746180
tekuto1437 <denisjerosenko79@gmail.com>
2021-03-23 15:22:35 +0200

    Task6

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,12 @@
-// Your JavaScript goes here.
+window.onload = function(){
+
+    var enterYourName = document.getElementById("full_name");
+  
+    enterYourName.addEventListener("keyup", function(){
+  
+      var greetings = document.getElementById("greeting");
+  
+      greetings.innerHTML = ("Greetings and salutations, " + enterYourName.value + "!");
+    });
+  
+  }; 
